d80d493486358d912c2c4624618cc1fe68dd4e68
Anna Sergacova <anna.sergacova@gmail.com>
2023-12-06 12:36:10 +0200

    .

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,7 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import HttpApi from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+
 import backend from "i18next-http-backend";
 i18n
 .use(initReactI18next) // passes i18n down to react-i18next
